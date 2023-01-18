cb83964c13ced06012c7bde43ebabb9d11f6839c
GOPI <gopinavadiya201199@gmail.com>
2023-01-18 20:41:22 +0530

    commit

---
+++ lib/model/user_model.dart
@@ -0,0 +1,9 @@
+class UserModel{
+  String? firstName;
+  String? lastName;
+  String? email;
+  String? password;
+  String? age;
+
+  UserModel({this.firstName,this.lastName,this.email, this.password, this.age});
+}
